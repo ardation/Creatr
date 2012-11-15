fa64a630052c5f2707b80aae31ef675c32fae10e
Reuben Posthuma <reuben.posthuma@gmail.com>
2012-11-16 00:08:05 +1300

    OG-44 merging

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ FlamingWight::Application.routes.draw do
   root :to => "site#index"
   match 'features' => "site#features"
   match 'testimonials' => "site#testimonials"
-  match 'pricing' => "#site#pricing"
+  match 'pricing' => "site#pricing"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
