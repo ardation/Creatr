5a0b5980e102866d6eb5676a0925221b1015f31d
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-11-17 01:35:57 +1300

    OG-54 controller flow

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -26,23 +26,24 @@ class SiteController < ApplicationController
 		    	session["devise.facebook_data"] = request.env["omniauth.auth"]
 		    	redirect_to '/activate'
 		    end
-	  	end
-	end
+      end
+    end
   end
 
   def signup_fb
   	@member = Member.find_for_facebook_oauth(session["devise.facebook_data"]) unless session["devise.facebook_data"].nil?
 
   	if @member.nil?
+      @member = Member.new
 	  	if session.exists? && !session["devise.facebook_data"].nil?
 		  	@name = session["devise.facebook_data"]['info']['name']  || ""
 		  	@email = if session["devise.facebook_data"]['info']['email'].include?("@facebook.com")
 		  		""
-			else
-				session["devise.facebook_data"]['info']['email']
-			end
-	  	else
-	    	redirect_to member_omniauth_authorize_path(:facebook)
+  			else
+  				session["devise.facebook_data"]['info']['email']
+  			end
+	  else
+	    redirect_to member_omniauth_authorize_path(:facebook)
 		end
 	else
 		redirect_to member_omniauth_authorize_path(:facebook)
@@ -58,7 +59,7 @@ class SiteController < ApplicationController
   	@member.tokenExpires = Time.at(session["devise.facebook_data"]['credentials']['expires_at'])
   	@member.uid = session["devise.facebook_data"]['uid']
   	@member.provider = session["devise.facebook_data"]['provider']
-  	
+
   	unless @member.save!
   		@name = params[:member][:name]
   		@email = params[:member][:email]
