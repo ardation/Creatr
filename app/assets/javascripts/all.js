148148a2d265d3a4404bf997d2e3eb8acde927a9
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-11-30 12:00:09 +1300

    asset pipeline fixes

---
+++ app/assets/javascripts/all.js
@@ -0,0 +1,6 @@
+//= require_directory ./controllers
+//= require_directory ./create
+//= require_directory ./dashboard
+//= require_directory ./models
+//= require_directory ./template
+//= require_directory ./views
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ FlamingWight::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( all.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
