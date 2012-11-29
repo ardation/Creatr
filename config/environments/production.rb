cf500ae7dc23cace6efd4abeabbd3f83de778a60
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-11-30 12:46:51 +1300

    fixed email bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ FlamingWight::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => ENV['app_url'] }
 end
