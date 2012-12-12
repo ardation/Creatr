8dcfda39b4d22e3d45fd922469f86260985d42ff
Reuben Posthuma <reuben.posthuma@gmail.com>
2012-12-12 20:57:43 +1300

    OG-72 getting the user client side app working, data upload working, increment button working

--- app/assets/javascripts/survey/survey.js
+++ app/assets/javascripts/survey/survey.js
@@ -1,12 +1,5 @@
 nextStep = 1;
 
-scratch = null;
-function sneakyFunction(data) {
-  console.log(data);
-  scratch = data;
-  return data;
-}
-
 subViews = new Array();
 content_types_obj = Ember.ArrayProxy.create({content: content_types});
 
@@ -44,11 +37,6 @@ for(i = 0; i<survey_contents.length; i++) {
   App.surveyControllers[i] = Ember.Controller.extend({
     id: i,
     next: this.id+1,
-    answer: App.SurveyData.content[this.id-1],
-
-    answerObserver: function() {
-      App.SurveyData.content[this.id-1] = this.answer;
-    }.observes('answer'),
 
     data: function() {
       fx = eval(content_type.js).data;
@@ -56,9 +44,29 @@ for(i = 0; i<survey_contents.length; i++) {
     }.property(),
 
     enter: function() {
-      alert("entered");
+      fx = eval(content_type.js).enter;
+      fx(this.readHelper, this.id, this.data, this);
+    },
+    
+    exit: function() {
+      fx = eval(content_type.js).exit;
+      fx(this.writeHelper, this.id, this.data, this);
+    },
+
+    writeHelper: function(index, data) {
+      App.SurveyData[index] = data;
+    },
+    
+    readHelper: function(index) {
+      return App.SurveyData[index];
+    },
+    _propertySet: function(name, data) {
+      App.surveyControllers[this.id].set(name, data);
     }
+
   });
+
+  App.surveyControllers[i] = App.surveyControllers[i].create();
 }
 
 App.ApplicationView = Ember.View.extend({
@@ -77,48 +85,51 @@ App.Router = Ember.Router.extend({
   enableLogging: true,
   root: Ember.Route.extend({
 
-    incrementStep: Ember.Route.transitionTo('step'),
+    incrementStep: function(router) {
+      router.transitionTo('step', {step:App.nextStep});
+    },
 
     index: Em.Route.extend({
       route: '/',
       connectOutlets: function(router) {
-        router.route('/1');
+        //router.route('//1');
+        test = {step:1};
+        router.transitionTo('step', {step:1});
       }
     }),
     step: Em.Route.extend({
       route: '/:step',
       connectOutlets: function(router, context) { 
-        if(context.step <= App.surveyViews.length) {
+        if(context.step*1 <= App.surveyViews.length) {
+          App.set('nextStep', context.step*1+1);
+          this.currentStep = context.step;
+          App.surveyControllers[context.step-1].enter();
           router.get('applicationController').connectOutlet({ 
               viewClass: App.surveyViews[context.step-1], 
-              controller: sneakyFunction(App.surveyControllers[context.step-1].create()),
+              controller: App.surveyControllers[context.step-1],
               context: {}
           });
-          router.controller.enter();
         }
         else {
           router.transitionTo('finish');
         }
       },
-      exit: function() {
-      //
-      }, 
-      enter: function() {
-
+      exit: function(router) {
+        //App.surveyControllers[this.currentStep-1].exit();
       }
     }),
     finish: Em.Route.extend({
       route: '/submit',
       connectOutlets: function(router, context) {
         router.get('applicationController').connectOutlet('submit');
+        App.SurveyData.uploadData();
       }
     })
   })
 });
 
 Ember.LOG_BINDINGS=true;
+
 $(document).ready(function() {
   App.initialize();
 });
-
-testAnswer = "Reuben";
