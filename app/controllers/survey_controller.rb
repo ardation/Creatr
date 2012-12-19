970b30890280edb985f8166be34b3e3536647296
Reuben Posthuma <reuben.posthuma@gmail.com>
2012-12-19 13:27:46 +1300

    OG-72 Updates

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -6,5 +6,6 @@ class SurveyController < ApplicationController
       @content_types << content.content_type unless @content_types.include?(content.content_type)
     end
     @content_types = @content_types.to_json(only: [:name, :js, :id])
+    # raise @survey.theme.css
   end
 end
