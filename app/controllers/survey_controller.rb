f2b688b433ee830f4e106c62abaee9c3e7cc2bfe
Tataihono Nikora <tataihono.nikora@gmail.com>
2012-12-19 19:42:03 +1300

    OG-73 Renamed surveys to campaigns

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,8 +1,8 @@
 class SurveyController < ApplicationController
   def index
-    @survey = Survey.find(params[:id])
+    @survey = Campaign.find(params[:id])
     @content_types = Array.new
-    @survey.contents.each do |content| 
+    @survey.contents.each do |content|
       @content_types << content.content_type unless @content_types.include?(content.content_type)
     end
     @content_types = @content_types.to_json(only: [:name, :js, :id])
