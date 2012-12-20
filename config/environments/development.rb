fda8eff5c79177cc5b6641a51843b2855efe8d5a
Josh Starcher <josh.starcher@gmail.com>
2012-12-19 21:20:21 -0500

    Don't raise email errors in dev mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ FlamingWight::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { :host => ENV['app_url'] }
 
