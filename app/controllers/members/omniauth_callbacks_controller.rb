b4a82fbe9c1a73d988dd6b85f77246dd3bc03bdc
Kewei Jiang <kewei.jiang@cru.org>
2018-02-07 12:28:15 +1300

    sign in fix

--- app/controllers/members/omniauth_callbacks_controller.rb
+++ app/controllers/members/omniauth_callbacks_controller.rb
@@ -4,6 +4,6 @@ class Members::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = Member.find_for_facebook_oauth(request.env['omniauth.auth'])
     session['devise.facebook_data'] = request.env['omniauth.auth'] if @user.nil?
-    sign_in_and_redirect @user, event: :authentication
+    redirect_to '/sign_in'
   end
 end
